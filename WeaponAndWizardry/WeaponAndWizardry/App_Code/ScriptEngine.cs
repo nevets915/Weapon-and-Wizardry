cc977cc85b098e10333e8061a4fda2b2616fc6c9
George <namblue@me.com>
2017-11-06 12:03:12 -0800

    Added ScriptEngine and basic functions PrintTextDialogue and SetChoiceButtons

---
+++ WeaponAndWizardry/WeaponAndWizardry/App_Code/ScriptEngine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace WeaponAndWizardry.App_Code
+{
+    /// <summary>
+    /// Handles execution of scenario scripts.
+    /// Exposes API for Script class to use.
+    /// </summary>
+    public partial class ScriptEngine
+    {
+        private int _currentExecutingLine;
+        private TextBox _display;
+        private List<ScriptLine> _scriptLines;
+        private List<Button> _choiceButtons;
+        private uint _choicePicked;
+        private delegate void ScriptLine();
+
+        /// <summary>
+        /// Instantiates an instance of the ScriptEngine
+        /// Requires a page as a parameter for interacting with the Page.
+        /// Scripts are run in the sequential order starting from the
+        /// first index in the collection.
+        /// </summary>
+        /// <param name="gui"></param>
+        public ScriptEngine(TextBox display, List<Button> choiceButtons)
+        {
+            _currentExecutingLine = 0;
+            _display = display;
+            _scriptLines = new List<ScriptLine>();
+            _choiceButtons = choiceButtons;
+            LoadScripts();
+        }
+
+        public void ExecuteLine(uint choicePicked)
+        {
+            _choicePicked = choicePicked;
+            _scriptLines[_currentExecutingLine].Invoke();
+        }
+
+        public void PrintTextDialogue(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+            _display.Text = message;            
+        }
+        
+        public void SetChoiceButtons(Choices choices)
+        {
+            for (int i = 0; i < _choiceButtons.Count; i++)
+            {
+                _choiceButtons[i].Enabled = true;
+                _choiceButtons[i].Text = choices.Collection[i];
+                if (String.IsNullOrWhiteSpace(_choiceButtons[i].Text))
+                {
+                    _choiceButtons[i].Enabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ WeaponAndWizardry/WeaponAndWizardry/App_Code/Scripts/Scripts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeaponAndWizardry.App_Code
+{
+    /// <summary>
+    /// Represents a script scenario that uses ScriptEngine API
+    /// to dictate the game flow.
+    /// </summary>
+    public partial class ScriptEngine
+    {
+        private void LoadScripts()
+        {
+            _scriptLines.Add(Line1);
+            _scriptLines.Add(Line2);
+        }
+
+        private void Line1()
+        {
+            SetChoiceButtons(new Choices("Fight", "Run"));
+            PrintTextDialogue("Enemy appears");
+            _currentExecutingLine++;
+        }
+        
+        private void Line2()
+        {
+            switch (_choicePicked)
+            {
+                case 1:
+                    SetChoiceButtons(new Choices());
+                    PrintTextDialogue("You attack!");
+                    break;
+                case 2:
+                    SetChoiceButtons(new Choices());
+                    PrintTextDialogue("You Run!");
+                    break;
+            }
+            _currentExecutingLine++;
+        }
+    }
+}
